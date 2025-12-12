dbcce0979ededab57d8ab9ff553a6f158b716cb6
Rajeev Rawat <rajeevrawat2100@email.com>
2025-12-12 18:21:38 +0530

    fix: usefetch api code

--- src/components/HotelByTitle.jsx
+++ src/components/HotelByTitle.jsx
@@ -1,7 +1,7 @@
 import useFetch from "../useFetch";
 
 const HotelByTitle = () => {
-  const { data, loading, error } = useFetch(`https://bi-1-3-hw-2-be.vercel.app/hotels/`);
+  const { data, loading, error } = useFetch(`https://bi-1-3-hw-2-be.vercel.app/hotels`);
   const filteredByTitle = data?.filter((title) => title.name === "Lake View");
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading books</p>;
